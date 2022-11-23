00c8dc003ced55553302804b1ae1c9121b5dac4f
MarcoSavarin0 <ateneo2003@gmail.com>
2022-11-23 20:55:18 -0300

    agregue un consolelog para sus respectivas funciones

--- resta.js
+++ resta.js
@@ -1,3 +1,5 @@
 function multiplicar (num1, num2){
     return num1 * num2
 }
+
+console.log(sumar(5,6))
--- sumar.js
+++ sumar.js
@@ -2,3 +2,4 @@ function sumar (num1, num2){
     return num1 + num2
 }
 
+console.log(sumar(5,6))
